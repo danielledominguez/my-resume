2dbf87fdd50befabf4f2b461e3e422657d8c0655
Danielle <ddoming@udel.edu>
2017-11-28 16:24:05 -0500

    databases added

--- manage.py
+++ manage.py
@@ -1,7 +1,18 @@
 from flask_script import Manager
-from resume import app
+from resume import app, db, Professor
 
 manager = Manager(app)
 
-if __name__ == '__main__':
+
+# reset the database and create two artists
+@manager.command
+def deploy():
+    db.drop_all()
+    db.create_all()
+    harrywang = Professor(name='Harry Wang', department='MIS')
+    db.session.add(harrywang)
+    db.session.commit()
+
+
+if __name__ == "__main__":
     manager.run()
